4e9c0834cd6090b3912f486f0890e9ce1cdd5e0a
Adam <crappyoats@gmail.com>
2016-07-29 12:39:05 -0500

    Starting revamped points system

--- bob_filter.py
+++ bob_filter.py
@@ -1,6 +1,8 @@
 from struct import unpack
 from collections import defaultdict
-from vtk import vtkUnstructuredGrid, vtkHexahedron, vtkPoints, vtkIntArray
+
+from vtk import (vtkUnstructuredGrid, vtkHexahedron, vtkPoints,
+                 vtkIntArray, vtkUnstructuredGridWriter)
 
 
 DX = 2.0
@@ -21,12 +23,11 @@ def read_chunk(bin_f):
 
 def make_cell(chunk, index):
     hexa = vtkHexahedron()
-
     for i in range(8):
         hexa.GetPointIds().SetId(i, index)
         index += 1
 
-    return hexa
+    return hexa, index
 
 
 def add_points(x, y, z, points):
@@ -45,25 +46,45 @@ def add_points(x, y, z, points):
     points.InsertNextPoint([scaled_x, scaled_y + DY, scaled_z + DZ])
 
 
+def write_vtk(ugrid):
+    writer = vtkUnstructuredGridWriter()
+    writer.SetFileName("test_filter.vtk")
+    writer.SetInput(ugrid)
+    writer.Write()
+
+
 def main(bin_f, filter_vals):
     read_file = open(bin_f, 'rb')
-    read_file.read(4)
 
     ugrid = vtkUnstructuredGrid()
     points = vtkPoints()
     values = vtkIntArray()
+
+    values.SetName("Filtered Values")
+    values.SetNumberOfComponents(1)
     index = 0
 
-    while bin_f.read(4):
-        chunk = read_chunk(bin_f)
+    while read_file.read(4):
+        chunk = read_chunk(read_file)
 
         if chunk['i'] in filter_vals:
             values.InsertNextTuple1(chunk['i'])
             add_points(chunk['x'], chunk['y'], chunk['z'], points)
 
-            cell = make_cell(chunk, index)
+            cell, index = make_cell(chunk, index)
             ugrid.InsertNextCell(cell.GetCellType(), cell.GetPointIds())
-        else:
-            continue
 
-        bin_f.read(4)
+        read_file.read(4)
+
+    read_file.close()
+
+    ugrid.SetPoints(points)
+    ugrid.GetCellData().AddArray(values)
+    write_vtk(ugrid)
+
+
+if __name__ == '__main__':
+    from sys import argv
+    filter_vals = range(8, 12)
+
+    main(argv[1], filter_vals)
